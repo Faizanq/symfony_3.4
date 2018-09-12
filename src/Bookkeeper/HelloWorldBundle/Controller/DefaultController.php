61614f33bb6650bbd08bbbbf3a48f4ad015d4a8c
faizanq <faizanqureshi0@gmail.com>
2018-09-12 22:59:23 +0530

    Defining our first route and action for HelloWorldBundle

--- src/Bookkeeper/HelloWorldBundle/Controller/DefaultController.php
+++ src/Bookkeeper/HelloWorldBundle/Controller/DefaultController.php
@@ -6,8 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('BookkeeperHelloWorldBundle:Default:index.html.twig');
+    // public function indexAction()
+    // {
+    //     return $this->render('BookkeeperHelloWorldBundle:Default:index.html.twig');
+    // }
+
+    public function indexAction(){
+    	return $this->render('BookkeeperHelloWorldBundle:Default:index.html.twig
+    		');
     }
 }
